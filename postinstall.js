a3bce9882450e532171aa5c921991dc72136d261
Moshe Kolodny <kolodny.github@gmail.com>
2016-04-10 22:47:13 -0400

    don't guess node_modules path

--- postinstall.js
+++ postinstall.js
@@ -1,8 +1,15 @@
 var fs = require('fs');
 var path = require('path');
 
-var root = path.resolve(__dirname + '/../../');
+var dirname = __dirname.replace(/\\/g, '/');
+var root = dirname.slice(0, dirname.lastIndexOf('/node_modules/'));
 var link = root + '/node_modules/~';
-if (!fs.existsSync(link)) {
+try {
+  var existingReal = fs.realpathSync(link);
+} catch (e) {
   fs.symlinkSync(root, link, 'junction');
+  process.exit(0);
+}
+if (existingReal && existingReal !== root) {
+  throw new Error(link + ' is already being used')
 }
